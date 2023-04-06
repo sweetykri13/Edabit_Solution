97aeee54879d2caf1fe31c8deb07f2e2386d950e
sweety <sweetykri17@gmail.com>
2023-04-06 16:34:07 +0530

    Bitwise Operation

---
+++ Bitwise_Operations.js
@@ -0,0 +1,14 @@
+function bitwiseAND(n1, n2) {
+	return n1 & n2;
+}
+
+function bitwiseOR(n1, n2) {
+	return n1 | n2;
+}
+
+function bitwiseXOR(n1, n2) {
+	return n1 ^ n2;
+}
+console.log(bitwiseAND(7,12));
+console.log(bitwiseOR(7,12));
+console.log(bitwiseXOR(7,12));
